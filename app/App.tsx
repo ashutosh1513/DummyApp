cd9646deeb2e17f9544910b9d75ca9bf2a478123
Ashutosh Mishra <ashutoshmishra4297@gmail.com>
2024-03-17 15:22:59 +0530

    initial commit

---
+++ app/App.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return <SafeAreaView>
+    <View style={{height:'100%', width:'100%', justifyContent:"center"}}>
+        <Text style={{textAlign:'center', justifyContent:'center'}}>
+          Hello World!
+        </Text>
+    </View>
+  </SafeAreaView>;
+}
+
+export default App;
